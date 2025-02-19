09e9576c96d12b49185d1443fa54f742635a1191
Kirillmlk <kostakov.kiirill@yandex.by>
2025-02-19 16:42:16 +0300

    Vue for sections and adding to the database

--- resources/js/Pages/Section/Index.vue
+++ resources/js/Pages/Section/Index.vue
@@ -1,13 +1,22 @@
 <template>
-<div>
-    index
-</div>
+    <div>
+        <div class="flex items-center">
+            <h3 class="text-xl mr-4">Разделы</h3>
+            <Link :href="route('sections.create')" class="block w-8 px-2 py-1 bg-white border border-gray-300 rounded-lg text-center">+</Link>
+        </div>
+    </div>
 </template>
 
 <script>
 import MainLayout from "@/Layouts/MainLayout.vue";
+import {Link} from "@inertiajs/vue3";
+
 export default {
     name: "Index",
+
+    components: {
+      Link,
+    },
 
     layout: MainLayout
 }
