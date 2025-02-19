6033a37b51916edb6ab77aa99e36389217a4ff34
Kirillmlk <kostakov.kiirill@yandex.by>
2025-02-19 15:58:06 +0300

    start project

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>
+
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
