281d8ae5e42a0cabfde2ebb3b9b95ede7e29bd67
Kirillmlk <kostakov.kiirill@yandex.by>
2025-02-27 20:16:32 +0300

    Processing and deleting old avatars

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,29 +2,45 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Http\Requests\User\UpdateRequest;
 use App\Http\Resources\User\UserResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Image;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver as GdDriver;
 
 class UserController extends Controller
 {
     public function personal()
     {
         $user = UserResource::make(auth()->user())->resolve();
-
         return inertia('User/Personal', compact('user'));
     }
+
 
     public function update(UpdateRequest $request)
     {
         $data = $request->validated();
+
+        // Удаление старого аватара, если он существует
+        if (auth()->user()->avatar) {
+            Storage::disk('public')->delete(auth()->user()->avatar);
+        }
+
+        // Сохранение нового аватара
         $path = Storage::disk('public')->put('/avatars', $data['avatar']);
-        auth()->user()->update([
-            'avatar' => $path
-        ]);
+
+        // Обновление пути в базе
+        auth()->user()->update(['avatar' => $path]);
+
+        // Инициализация ImageManager с GdDriver
+        $manager = new ImageManager(new GdDriver());
+
+        // Читаем изображение, ресайзим и сохраняем
+        $image = $manager->read(storage_path("app/public/{$path}"))->cover(95, 95);
+        $image->save();
 
         return UserResource::make(auth()->user())->resolve();
     }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable
 
     public function getAvatarUrlAttribute()
     {
-        return url('storage/' . $this->avatar);
+        return  $this->avatar ? url('storage/' . $this->avatar) : null;
     }
 }
