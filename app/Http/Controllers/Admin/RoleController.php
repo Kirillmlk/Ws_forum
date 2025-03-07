ea0ccd379f8dc0b33e93c2d8c8f22257eeb359de
Kirillmlk <kostakov.kiirill@yandex.by>
2025-03-07 21:12:48 +0300

    Admin Panel Role Publishing

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Role\StoreRequest;
+use App\Http\Resources\Section\SectionResource;
+use App\Models\Role;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
@@ -14,6 +18,29 @@ class RoleController extends Controller
 
     public function create()
     {
-        return inertia('Admin/Role/Create');
+        $sections = Section::all();
+        $sections = SectionResource::collection($sections)->resolve();
+
+        return inertia('Admin/Role/Create', compact('sections'));
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $code = $data['title'];
+
+        if ($data['branch_id']) {
+            $code .= '.' . $data['section_id'] . '.' . $data['branch_id'];
+        } elseif (!$data['branch_id'] && $data['section_id']) {
+            $code .= '.' . $data['section_id'];
+        }
+
+        Role::firstOrCreate([
+           'title' => $data['title'],
+           'code' => $code,
+        ]);
+
+        return redirect()->route('admin.roles.index');
     }
 }
